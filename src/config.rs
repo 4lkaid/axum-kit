5fcc099d70a4dd94898361db20d1c94e37b0dd0c
4lkaid <4lkaid@gmail.com>
2024-11-17 10:56:25 +0800

    feat: add custom deserialization macro with context

--- src/config.rs
+++ src/config.rs
@@ -4,11 +4,33 @@ use crate::{
 use anyhow::Result;
 use serde::Deserialize;
 
+macro_rules! deserialize_with_context {
+    ($name:ident, $type:ty, $context:expr) => {
+        fn $name<'de, D>(deserializer: D) -> std::result::Result<$type, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            let config = <$type>::deserialize(deserializer)
+                .map_err(|e| serde::de::Error::custom(format!("{}: {}", $context, e)))?;
+            Ok(config)
+        }
+    };
+}
+
+deserialize_with_context!(deserialize_general_config, GeneralConfig, "[general]");
+deserialize_with_context!(deserialize_logger_config, LoggerConfig, "[logger]");
+deserialize_with_context!(deserialize_postgres_config, PostgresConfig, "[postgres]");
+deserialize_with_context!(deserialize_redis_config, RedisConfig, "[redis]");
+
 #[derive(Debug, Deserialize)]
 pub struct Config {
+    #[serde(deserialize_with = "deserialize_general_config")]
     pub general: GeneralConfig,
+    #[serde(deserialize_with = "deserialize_logger_config")]
     pub logger: LoggerConfig,
+    #[serde(deserialize_with = "deserialize_postgres_config")]
     pub postgres: PostgresConfig,
+    #[serde(deserialize_with = "deserialize_redis_config")]
     pub redis: RedisConfig,
 }
 
